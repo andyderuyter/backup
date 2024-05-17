0894b53e73a0efd9c2290714f402ea73476986e5
Andy De Ruyter <deruyterandy@gmail.com>
2024-05-17 16:07:10 +0200

    Update backup.sh
    Added timestamp and changed filenames of generated archives

--- backup.sh
+++ backup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-TODAY=$(date '+%Y%m%d')
+NOW=$(date '+%d%m%Y-%H%M%S')
 
 # Username
 read -p "Enter username: " USERNAME
@@ -40,15 +40,15 @@ echo
 echo "Starting Backup..."
 
 # Database backup (mysql)
-mysqldump -h "$DATABASEHOST" -u "$DB_USER" -p"$DATABASEPASSWORD" "$DB_NAME" --no-tablespaces > "$TEMP_DIR/$DATABASENAME.sql"
+mysqldump -h "$DATABASEHOST" -u "$DB_USER" -p"$DATABASEPASSWORD" "$DB_NAME" --no-tablespaces > "$TEMP_DIR/$DATABASENAME-$NOW.sql"
 
 # Backup site files
-tar -zcf "$TEMP_DIR/files.tar.gz" -C "$SITE_PATH" .
+tar -zcf "$TEMP_DIR/public_html.tar.gz" -C "$SITE_PATH" .
 
 # Create archive
-tar -zcf "$BACKUP_DIR/$BACKUP_NAME-$TODAY.tar.gz" -C "$TEMP_DIR" .
+tar -zcf "$BACKUP_DIR/$BACKUP_NAME-$NOW.tar.gz" -C "$TEMP_DIR" .
 
 # Delete temporary files
 rm -Rf "$TEMP_DIR"
 
-echo "Backup Complete [$(du -sh "$BACKUP_DIR/$BACKUP_NAME-$TODAY.tar.gz" | awk '{print $1}')]"
+echo "Backup Complete [$(du -sh "$BACKUP_DIR/$BACKUP_NAME-$NOW.tar.gz" | awk '{print $1}')]"
